7a6daf2969a0333d03850fe74b74870c12518edd
Douglas Sales Amante <douglassales@gec.inatel.br>
2019-11-22 19:40:23 -0300

    Update index.js

--- Scheme/index.js
+++ Scheme/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-var MemeSchema = new mongoose.MemeSchema({
+var MemeSchema = new Schema({
     titulo : { type : String, required : true},
     descricao : { type : String, required : true},
     ano : {type : Number}
