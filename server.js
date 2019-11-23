1462c1e9ef846f9e1535d79dff168b1db0801a70
Douglas Sales Amante <douglassales@gec.inatel.br>
2019-11-23 14:56:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 (async()=>{
+
     const restify = require('restify');
     const mongoose = require('mongoose');
 
@@ -9,35 +10,54 @@
         useFindAndModify: false,
         useUnifiedTopology: true,
     };
-
+    //It will try do a conexion with the Mongodb 
     try {
+        //do not there is error. Conexion completed with mongodb 
         await mongoose.connect(DB_URL, DB_SETTINGS);
-        console.log('Conexao concluida com o mongodb');
+        console.log('Conexion completed with the mongodb');
     } catch (e) {
-        console.error('Erro na conexao com o mongodb:${DB_URL');
+        //there is an error
+        console.error('Error in the conexion with the mongodb');
         console.error(e.message);
     }
+
+    //server name used equal "AdautoWork"
+
     var server = restify.createServer({
         name: 'AdautoWork'
     });
 
+    //configuration to server in relation body and query 
     server.use(restify.plugins.bodyParser());
     server.use(restify.plugins.queryParser());
     
 
     const MemeController = require('./Controller/meme');
+
     const AuthMiddleware = require('./Middleware/middle');
+
     const {error404} = require('./Middleware/error');
 
     const AuthController = require('./Controller/auth');
 
+
+    //all available routes to application in the server
+
+    //login route
     server.post('/auth/login',AuthController.login);
+
+    //search route
     server.get('/meme', AuthMiddleware.validateToken, MemeController.getRead);
+    //search route with id
     server.get('/meme/:id', AuthMiddleware.validateToken, MemeController.getById);
+    //create route
     server.post('/meme', AuthMiddleware.validateToken, MemeController.create);
-    server.patch('/meme', AuthMiddleware.validateToken, MemeController.update);
+    //update route 
+    server.patch('/meme/:id', AuthMiddleware.validateToken, MemeController.update);
+    //delete route
     server.del('/meme', AuthMiddleware.validateToken, MemeController.deleteMeme);
 
+    //port used to server
     const PORT = process.env.PORT || 3000;
 
     server.listen(PORT);
