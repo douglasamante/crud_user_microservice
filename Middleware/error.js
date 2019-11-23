38f6ce0d6736bb2eada274170bde900be5c4f777
Douglas Sales Amante <douglassales@gec.inatel.br>
2019-11-23 14:59:44 -0300

    Update error.js

--- Middleware/error.js
+++ Middleware/error.js
@@ -1,7 +1,8 @@
 const errs = require('restify-errors');
 
+//as the function 404 will catch the error.
 function error404(request, response){
-    return next (new errs.NotFoundError('rota nao encontrada'));
+    return next (new errs.NotFoundError('Route does not find'));
 }
 
 module.exports = {
