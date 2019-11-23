8953bc0ffc9e8d31a140e7becf2597d26a8fc08b
Douglas Sales Amante <douglassales@gec.inatel.br>
2019-11-23 14:57:55 -0300

    Update middle.js

--- Middleware/middle.js
+++ Middleware/middle.js
@@ -1,25 +1,33 @@
 const errs = require('restify-errors');
-const axios = require('axios');
-const axiosInstance = axios.create({ 
-});
 
+const axios = require('axios');
+
+const axiosInstance = axios.create();
+
+//Url used to application
 const baseURL = 'https://ec021-2019-2-av2-auth.herokuapp.com/auth/validateToken';
 
+//function to validate the token
 function validateToken(req, res, next) {
     const { token } = req.headers;
-    if (!token) return next(new errs.ForbiddenError('Token não fornecido'));
+
+    if (!token) return next(new errs.ForbiddenError('Token did not provide'));
+
     axios.post(baseURL, {}, {
         headers: {
             token
         }
     })
-        .then((response) => {
+    .then((response) => {
             next()
-        })
-        .catch((error) => {
+    })
+    
+    .catch((error) => {
+            //Write of the error in relation in the token
             console.log(error);
-                return res.send({menseger:'Token invalido'})
-        });
+
+                return res.send({menseger:'Token invalid'})
+    });
     
 }
 
