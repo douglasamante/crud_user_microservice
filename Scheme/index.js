c64fcab411eb883804a585e87db2f856f48b2d04
Douglas Sales Amante <douglassales@gec.inatel.br>
2019-11-23 14:59:11 -0300

    Update index.js

--- Scheme/index.js
+++ Scheme/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//describe the memeSchema in relation the params(title, describe, year)
 var MemeSchema = new Schema({
     titulo : { type : String, required : true},
     descricao : { type : String, required : true},
@@ -8,5 +9,8 @@ var MemeSchema = new Schema({
 },  {
     timestamps: true
 });
+
+
+//export to module
 
 module.exports = mongoose.model('Meme', MemeSchema);
