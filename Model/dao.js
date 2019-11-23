7f1df0a16ec1280c5883b0a67e0a2f6aa40cc65a
Douglas Sales Amante <douglassales@gec.inatel.br>
2019-11-23 14:58:43 -0300

    Update dao.js

--- Model/dao.js
+++ Model/dao.js
@@ -1,15 +1,18 @@
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
 
+//uri used 
 const uri = 'mongodb+srv://adauto:adauto@cluster0-rven8.mongodb.net/test?retryWrites=true&w=majority';
 const params = {
     useNewUrlParser: true,
     useUnifiedTopology: true
 };
 
+//configuration in relation the documentation proposed
 const dbName = 'ec021-av2-core';
 const collName = 'memes';
 
+//export the module
 module.exports = {
     uri,
     params
