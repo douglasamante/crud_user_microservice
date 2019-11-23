dc7af3ad36ebfbab4a4d5bc682a82df14e7a5606
Douglas Sales Amante <douglassales@gec.inatel.br>
2019-11-23 14:57:30 -0300

    Update auth.js

--- Controller/auth.js
+++ Controller/auth.js
@@ -1,23 +1,25 @@
 const axios = require('axios');
-const axiosInstace = axios.create({
-});
+const axiosInstace = axios.create();
+
+//URL used
 
 const baseURL = 'https://ec021-2019-2-av2-auth.herokuapp.com/auth/login';
 
 function login(req, res) {
+    //data used to login(username and password)
     let { username, password } = req.body;
-    //console.log(`${username} | ${password}`);
-    //console.log(`${req.body}`);
+  
     let config = {
         headers: {},
     };
+
     axios.post(baseURL, { username, password }, config)
     .then((response) => {
-        //console.log(response);
+       //return in relation the response
         return res.json(response.data);
     })
     .catch((error) => {
-        //console.error(error);
+       //return in relation the error
         return res.json(error);
     });
 }
